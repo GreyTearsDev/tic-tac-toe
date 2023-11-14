6afb0ca1114432a16e45e95c52ba535193043d07
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2023-11-14 19:51:52 +0100

    Refactor the grid array

--- script.js
+++ script.js
@@ -1,51 +1,101 @@
 const log = console.log;
 
-const createGameBoard = (function () {
-  let x0 = [0, 1, 2];
-  let x1 = [0, 1, 2];
-  let x2 = [0, 1, 2];
+const createGameBoard = function () {
+  const x = [0, 1, 2]; // rows
+  const y = [0, 1, 2]; //columns
 
-  const grid = [x0, x1, x2];
+  const winLocations = {
+    firstRow: [
+      [x[0], y[0]],
+      [x[0], y[1]],
+      [x[0], y[2]],
+    ],
+    secondRow: [
+      [x[1], y[0]],
+      [x[1], y[1]],
+      [x[1], y[2]],
+    ],
+    thirdRow: [
+      [x[0], y[0]],
+      [x[1], y[1]],
+      [x[2], y[2]],
+    ],
+    firstColumn: [
+      [x[0], y[0]],
+      [x[1], y[0]],
+      [x[2], y[0]],
+    ],
+    secondColumn: [
+      [x[0], y[1]],
+      [x[1], y[1]],
+      [x[2], y[1]],
+    ],
+    firstColumn: [
+      [x[0], y[2]],
+      [x[1], y[2]],
+      [x[2], y[2]],
+    ],
+    leftDiagonal: [
+      [x[0], y[0]],
+      [x[1], y[1]],
+      [x[2], y[2]],
+    ],
+    rightDiagonal: [
+      [x[0], y[2]],
+      [x[1], y[1]],
+      [x[2], y[0]],
+    ],
+  };
 
-  winningLocations = [
-    [x0[0], x0[1], x0[2]],
-    [x1[0], x1[1], x1[2]],
-    [x2[0], x2[1], x2[2]],
-    [x0[0], x1[0], x2[0]],
-    [x0[1], x1[1], x2[1]],
-    [x0[2], x1[2], x2[2]],
-    [x0[0], x1[1], x2[2]],
-    [x0[2], x1[1], x2[0]],
+  const grid = [
+    winLocations.firstRow,
+    winLocations.secondRow,
+    winLocations.thirdRow,
   ];
 
-  return { grid, winningLocations };
-})();
+  return { grid, winLocations };
+};
 
-function createPlayer() {
-  const name = player;
+function createPlayer(playerName) {
+  const name = playerName;
   let score = 0;
-  let moves = []; // stores the position of the moves the user makes
+  let locations = []; // stores the locations where the user has been to
   let moveCount = 0; // keeps track of how many times the user has moved
 
   const declareRoundWinner = () => score++;
-  const move = (location) => moves.push(location);
+  const move = (location) => locations.push(location);
   const getScore = () => score;
   const getName = () => name;
   const setName = (newName) => (name = newName);
 
-  return { getName, setName, getScore, move, moveCount, declareRoundWinner };
+  return {
+    getName,
+    setName,
+    getScore,
+    move,
+    moveCount,
+    locations,
+    declareRoundWinner,
+  };
 }
 
 const game = (function () {
-  let player1 = createPlayer();
-  let player2 = createPlayer();
+  let player1 = createPlayer("X");
+  let player2 = createPlayer("O");
   let roundCount = 0;
+  const gameBoard = createGameBoard();
 
   const setPlayerName = () => {
     player1.setName(prompt("what is your name?"));
     player2.setName(prompt("what is your name?"));
   };
 
+  const checkForWinner = (player) => {
+    for (let locationArray of gameBoard.winLocations) {
+      log("winner found");
+    }
+    log("no winner yet");
+  };
   let start = () => {};
 
   return {
@@ -55,5 +105,12 @@ const game = (function () {
     setPlayerName,
     start,
     roundCount,
+    checkForWinner,
   };
 })();
+
+game.player1.locations.push(2);
+game.player1.locations.push(1);
+game.player1.locations.push(0);
+
+game.checkForWinner(game.player1);
