0a688cf4f8e3496bc3842e423701e37b626ecc22
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2023-11-15 11:35:31 +0100

    Start working on the logic of the game
    create a play function which will be used to run the entire game

--- script.js
+++ script.js
@@ -1,57 +1,66 @@
 const log = console.log;
 
 const createGameBoard = function () {
-  const x = [0, 1, 2]; // rows
-  const y = [0, 1, 2]; //columns
+  const firstRow = [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+  ];
+  const secondRow = [
+    [1, 0],
+    [1, 1],
+    [1, 2],
+  ];
+  const thirdRow = [
+    [0, 0],
+    [1, 1],
+    [2, 2],
+  ];
 
   const winLocations = {
     firstRow: [
-      [x[0], y[0]],
-      [x[0], y[1]],
-      [x[0], y[2]],
+      [0, 0],
+      [0, 1],
+      [0, 2],
     ],
     secondRow: [
-      [x[1], y[0]],
-      [x[1], y[1]],
-      [x[1], y[2]],
+      [1, 0],
+      [1, 1],
+      [1, 2],
     ],
     thirdRow: [
-      [x[0], y[0]],
-      [x[1], y[1]],
-      [x[2], y[2]],
+      [0, 0],
+      [1, 1],
+      [2, 2],
     ],
     firstColumn: [
-      [x[0], y[0]],
-      [x[1], y[0]],
-      [x[2], y[0]],
+      [0, 0],
+      [1, 0],
+      [2, 0],
     ],
     secondColumn: [
-      [x[0], y[1]],
-      [x[1], y[1]],
-      [x[2], y[1]],
+      [0, 1],
+      [1, 1],
+      [2, 1],
     ],
     firstColumn: [
-      [x[0], y[2]],
-      [x[1], y[2]],
-      [x[2], y[2]],
+      [0, 2],
+      [1, 2],
+      [2, 2],
     ],
     leftDiagonal: [
-      [x[0], y[0]],
-      [x[1], y[1]],
-      [x[2], y[2]],
+      [0, 0],
+      [1, 1],
+      [2, 2],
     ],
     rightDiagonal: [
-      [x[0], y[2]],
-      [x[1], y[1]],
-      [x[2], y[0]],
+      [0, 2],
+      [1, 1],
+      [2, 0],
     ],
   };
 
-  const grid = [
-    winLocations.firstRow,
-    winLocations.secondRow,
-    winLocations.thirdRow,
-  ];
+  const grid = [firstRow, secondRow, thirdRow];
 
   return { grid, winLocations };
 };
@@ -79,10 +88,6 @@ function createPlayer(playerName) {
 }
 
 const game = (function () {
-  let player1 = createPlayer("X");
-  let player2 = createPlayer("O");
-  let roundCount = 0;
-  let totalMovements = 0;
   const gameBoard = createGameBoard();
 
   const setPlayerName = () => {
@@ -90,27 +95,76 @@ const game = (function () {
     player2.setName(prompt("what is your name?"));
   };
 
+  const getDesiredLocation = (rowIndex, locationIndex) => {
+    return gameBoard.grid[rowIndex].splice(locationIndex, 1);
+  };
+  const movePlayer = (player, location) => {
+    player.locations.push(location);
+  };
+
   const playerWonTheGame = (player) => {
     for (let property in gameBoard.winLocations) {
       let winCondition = gameBoard.winLocations[property];
       log(player.locations);
       if (player.locations.includes(winCondition)) {
+        player.score++;
         return true;
       }
       return false;
     }
   };
 
+  const displayWinner = (player) => log(`${player} won the game`);
+
+  // put it in a while loop to be checked every time someone plays from the moment the totalMoveCount reaches 5
   const checkForTheWinner = (player1, player2) => {
     // checks if a player has won the game. If true, it returns the player. Otherwise, it returns undefined
     if (playerWonTheGame(player1)) return player1;
     if (playerWonTheGame(player2)) return player2;
-
-    return undefined;
   };
 
-  let start = () => {
-    roundCount++;
+  let play = () => {
+    let totalMoveCount = 0;
+    let grid = gameBoard.grid;
+    let player1 = createPlayer("X");
+    let player2 = createPlayer("O");
+    let roundCount = 1;
+    let gameWinner;
+
+    log(`round: ${roundCount++}`);
+    for (let i = 5; totalMoveCount >= i; i--) {
+      let player1Move = prompt("X enter a location");
+      movePlayer(player1, player1Move);
+      let player2Move = prompt("O enter a location");
+      movePlayer(player2, player2Move);
+    }
+
+    while (checkForTheWinner(player1, player2) === undefined) {
+      let player1Move = prompt("X enter a location");
+      movePlayer(player1, player1Move);
+      if (playerWonTheGame(player1)) {
+        displayWinner(player1);
+        break;
+      }
+
+      let player2Move = prompt("O enter a location");
+      movePlayer(player2, player2Move);
+      if (playerWonTheGame(player2)) {
+        displayWinner(player2);
+        break;
+      }
+    }
+
+    if (roundCount == 2) {
+      player1.score > player2.score
+        ? (gameWinner = player1)
+        : (gameWinner = player2);
+    }
+    if (roundCount == 3) {
+      player1.score > player2.score
+        ? (gameWinner = player1)
+        : (gameWinner = player2);
+    }
   };
 
   return {
@@ -118,16 +172,20 @@ const game = (function () {
     player2,
     gameBoard,
     setPlayerName,
-    start,
+    play,
     roundCount,
     checkForTheWinner,
+    movePlayer,
+    getDesiredLocation,
   };
 })();
 
-game.player1.locations.push(game.gameBoard.winLocations.firstRow);
+game.play();
 
-if (game.checkForTheWinner(game.player1, game.player2)) {
-  log("win condition was found");
-} else {
-  log("something whent wrong");
-}
+// let location = game.getDesiredLocation(0, 0);
+// game.movePlayer(game.player1, location);
+// location = game.getDesiredLocation(1, 2);
+// game.movePlayer(game.player1, location);
+// log(game.player1.locations);
+
+//to do in ui: give each grid cell a tag representing its row and location within the row index
