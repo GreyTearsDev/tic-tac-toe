b346bf57758e4ccfab479b5a099e9f5c51d717a1
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2023-11-19 18:41:17 +0100

    Define setters and getters for the player moveCount

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const log = console.log;
 
 const createGameBoard = function () {
-  const winLocations = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
+  const winLocations = {
+    firstRow: [0, 1, 2],
+    secondRow: [3, 4, 5],
+    thirdRow: [6, 7, 8],
+    firstColumn: [0, 3, 6],
+    secondColumn: [1, 4, 7],
+    thirdColumn: [2, 5, 8],
+    diagonalLeft: [0, 4, 8],
+    diagonalRight: [2, 4, 6],
+  };
 
   const grid = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
@@ -18,30 +18,33 @@ const createGameBoard = function () {
 };
 
 let createPlayer = function () {
-  let name;
   let score = 0;
   let locations = []; // stores the locations where the user has been to
   let canMove; // keeps track of whether it is the users turn to play (true if yes. false if no)
+  let moveCount = 0;
 
   const move = (location) => locations.push(location);
+  const increaseMoveCount = () => {
+    moveCount++;
+  };
+  const getMoveCount = () => {
+    return moveCount;
+  };
   const getScore = () => score;
-  const getName = () => name;
-  const setName = (newName) => (name = newName);
 
   return {
     canMove,
-    getName,
-    setName,
     getScore,
     locations,
     move,
+    getMoveCount,
+    increaseMoveCount,
   };
 };
 
 const game = (function () {
   const gameBoard = createGameBoard();
   let roundCount = 1;
-  let moveCount = 0;
   let gameWinner;
   let roundWinner;
   let player1 = createPlayer("X");
@@ -55,34 +58,38 @@ const game = (function () {
 
   const playerWonTheRound = (player) => {
     let winLocations = gameBoard.winLocations;
-    log(player.locations);
-    for (let i = 0; i < winLocations.length; i++) {
-      log(winLocations[i]);
-      log(player.locations.includes(winLocations[i]));
-      if (player.locations.includes(winLocations[i])) {
-        log("hi");
-        player.score++;
-        return true;
-      }
+    log("won?");
+    for (let winLoc in winLocations) {
+      log(winLoc);
     }
-    return false;
+
+    // for (let location in winLocations) {
+    //   log(location);
+    //   if (player.locations.includes(winLocations[location])) {
+    //     log("hi");
+    //     player.score++;
+    //     return true;
+    //   }
+    // }
+    // return false;
   };
 
   const movePlayer = (player1, player2, location) => {
     getDesiredLocation(location);
 
     if (player1.canMove) {
+      player1.increaseMoveCount();
+      log(player1.getMoveCount());
       player1.move(location);
       player1.canMove = false;
       player2.canMove = true;
-      moveCount++;
     } else {
+      player2.increaseMoveCount();
+      log(player2.getMoveCount());
       player2.move(location);
       player2.canMove = false;
       player1.canMove = true;
-      moveCount++;
     }
-    log(gameBoard.grid);
   };
 
   const resetGameData = (player1, player2) => {
@@ -93,26 +100,13 @@ const game = (function () {
     gameBoard.grid = [0, 1, 2, 3, 4, 5, 6, 7, 8];
   };
 
-  // put it in a while loop to be checked every time someone plays from the moment the totalMoveCount reaches 5
-  const checkForTheWinner = (player1, player2) => {
-    // checks if a player has won the game. If true, it returns the player. Otherwise, it returns undefined
-    if (playerWonTheRound(player1)) return player1;
-    if (playerWonTheRound(player2)) return player2;
-  };
-
-  const gameStatus = () => {
-    if (moveCount >= 5) {
-      roundWinner = checkForTheWinner(player1, player2);
-    }
-  };
   return {
     roundCount,
-    moveCount,
     movePlayer,
     player1,
     player2,
     gameBoard,
-    checkForTheWinner,
+    playerWonTheRound,
   };
 })();
 
@@ -126,10 +120,19 @@ const game = (function () {
 
       if (game.gameBoard.grid.includes(Number(cellId))) {
         if (game.player1.canMove === true) {
+          if (game.player1.moveCount >= 3) {
+            log("hi");
+            playerWonTheRound(player1);
+          }
+
           game.movePlayer(game.player1, game.player2, cellId);
           cell.textContent = "X";
           cell.style.color = "#044040";
         } else {
+          if (game.player2.moveCount >= 3) {
+            playerWonTheRound(player2);
+          }
+
           game.movePlayer(game.player1, game.player2, cellId);
           cell.textContent = "O";
           cell.style.color = "#8c1f28";
@@ -161,11 +164,3 @@ const game = (function () {
     }
   });
 })();
-
-// let location = game.getDesiredLocation(0, 0);
-// game.movePlayer(game.player1, location);
-// location = game.getDesiredLocation(1, 2);
-// game.movePlayer(game.player1, location);
-// log(game.player1.locations);
-
-//to do in ui: give each grid cell a tag representing its row and location within the row index
